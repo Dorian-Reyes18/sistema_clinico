2c8f75963d8051bb3e226246d564088293b8697a
Dorian Reyes <doreyes@gbm.net>
2024-09-19 11:12:08 -0600

    Creando endpoint de metadata

---
+++ src/app/api/protected/metadata/route.js
@@ -0,0 +1,52 @@
+import { NextResponse } from "next/server";
+import { prisma } from "@/libs/prisma";
+import { authenticateRequest } from "@/middlewares/authMiddleware";
+
+// Función auxiliar para manejar errores
+const handleError = (error, message, status = 500) => {
+  console.error(message, error);
+  return NextResponse.json({ error: message }, { status });
+};
+
+// Función auxiliar para manejar autenticación y operaciones CRUD
+const handleRequest = async (req, operation) => {
+  const authResult = await authenticateRequest(req);
+  if (authResult) return authResult; // Si la autenticación falla, devuelve la respuesta de error
+
+  try {
+    return await operation(); // Si la autenticación pasa, ejecuta la operación
+  } catch (error) {
+    return handleError(error, "Error al procesar la solicitud");
+  }
+};
+
+// Manejar la solicitud GET para obtener los modelos
+export async function GET(req) {
+  return handleRequest(req, async () => {
+    try {
+      // Aquí obtenemos los datos de todos los modelos que mencionaste:
+      const categorias = await prisma.categoria.findMany();
+      const tipoDefectos = await prisma.tipoDefecto.findMany();
+      const silais = await prisma.silais.findMany();
+      const departamentos = await prisma.departamento.findMany();
+      const municipios = await prisma.municipio.findMany();
+      const sangreRH = await prisma.sangreRH.findMany();
+      const tipoDiabetes = await prisma.tipoDiabetes.findMany();
+      const etapaCirugia = await prisma.etapaCirugia.findMany();
+
+      // Devolveremos todos los modelos en un solo objeto
+      return NextResponse.json({
+        categorias,
+        tipoDefectos,
+        silais,
+        departamentos,
+        municipios,
+        sangreRH,
+        tipoDiabetes,
+        etapaCirugia,
+      });
+    } catch (error) {
+      return handleError(error, "Error al obtener los datos", 500);
+    }
+  });
+}
--- src/app/api/protected/usuarios/route.js
+++ src/app/api/protected/usuarios/route.js
@@ -1,21 +1,15 @@
 import { NextResponse } from "next/server";
 import { prisma } from "@/libs/prisma";
-import bcrypt from "bcryptjs";
 import { authenticateRequest } from "@/middlewares/authMiddleware";
-import { singToken } from "@/libs/jwt";
 
-
-// Función auxiliar para manejar errores
 const handleError = (error, message, status = 500) => {
   console.error(message, error);
-  // Asegúrate de enviar una respuesta JSON válida
   return NextResponse.json({ error: message }, { status });
 };
 
-// Función auxiliar para manejar autenticación y operaciones CRUD
 const handleRequest = async (req, operation) => {
   const authResult = await authenticateRequest(req);
-  if (authResult) return authResult; // Retorna el error si la autenticación falla
+  if (authResult) return authResult;
 
   try {
     return await operation();
@@ -24,65 +18,30 @@ const handleRequest = async (req, operation) => {
   }
 };
 
-// Crear usuario
-// Crear usuario
-export async function POST(req) {
-  return handleRequest(req, async () => {
-    try {
-      const { rolId, usuario, nombreYApellido, telefono, contrasena } =
-        await req.json();
-
-      const existingUser = await prisma.usuarios.findUnique({
-        where: { telefono },
-      });
-
-      if (existingUser) {
-        return NextResponse.json(
-          { error: "El número de teléfono ya está en uso." },
-          { status: 400 }
-        );
-      }
-
-      const hashedPassword = await bcrypt.hash(contrasena, 10);
-
-      const nuevoUsuario = await prisma.usuarios.create({
-        data: {
-          rolId,
-          usuario,
-          nombreYApellido,
-          telefono,
-          contrasena: hashedPassword,
-        },
-      });
-
-      if (!nuevoUsuario) {
-        return NextResponse.json(
-          {
-            error: "Error al crear el usuario. Por favor, inténtelo de nuevo.",
-          },
-          { status: 500 }
-        );
-      }
-
-      const token = singToken(nuevoUsuario); // Crear token JWT después de crear el usuario
-      return NextResponse.json({
-        message: "Usuario creado exitosamente",
-        usuario: nuevoUsuario,
-        token,
-      });
-    } catch (error) {
-      return handleError(
-        error,
-        "Error al procesar la solicitud. Asegúrese de que todos los datos sean correctos."
-      );
-    }
-  });
-}
-
-// Obtener todos los usuarios
 export async function GET(req) {
   return handleRequest(req, async () => {
-    const usuarios = await prisma.usuarios.findMany();
-    return NextResponse.json(usuarios);
+    try {
+      const categorias = await prisma.categoria.findMany();
+      const tipoDefectos = await prisma.tipoDefecto.findMany();
+      const silais = await prisma.silais.findMany();
+      const departamentos = await prisma.departamento.findMany();
+      const municipios = await prisma.municipio.findMany();
+      const sangreRH = await prisma.sangreRH.findMany();
+      const tipoDiabetes = await prisma.tipoDiabetes.findMany();
+      const etapaCirugia = await prisma.etapaCirugia.findMany();
+
+      return NextResponse.json({
+        categorias,
+        tipoDefectos,
+        silais,
+        departamentos,
+        municipios,
+        sangreRH,
+        tipoDiabetes,
+        etapaCirugia,
+      });
+    } catch (error) {
+      return handleError(error, "Error al obtener los datos", 500);
+    }
   });
 }
