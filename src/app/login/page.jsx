607326fb37b39cded4a04c66e09470d85c4df7b4
Dorian Reyes <doreyes@gbm.net>
2024-09-25 23:17:27 -0600

    Diseñando el login

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -3,7 +3,19 @@ const LoginPage = () => {
   return (
     <div className="login">
       <div className="card-login">
-        <div className="col-lf"></div>
+        <div className="col-lf">
+          <div className="content">
+            <strong>
+              <h1 style={{ color: "#fff" }}>Bienvenido</h1>
+            </strong>
+            <strong>
+              <p style={{ color: "#fff" }}>
+                Sistema Gestor <br />
+                base de cirugías
+              </p>
+            </strong>
+          </div>
+        </div>
         <div className="col-rg">
           <div className="cont-login">
             <Login />
